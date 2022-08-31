dc7ecf036ebe8a759ad067fe957c8033b31c2a6b
Cesar Ortiz <ceazthemoment@aol.com>
2022-08-31 04:35:00 -0400

    edit db image

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,18 +1,18 @@
 \c meta_mma;
 
 INSERT INTO meta (name, wins, loss, age, height, weight, reach, born, image) VALUES
-('Alexander Volkanovski', 25, 1, 33, 66, 144, 71, 'Australia', 'AlexVolkmma.png'),
-('Israel Adesanya', 23, 1, 32, 76, 185, 80, 'Nigeria', 'AlexVolkmma.png'),
-('Charles Oliveira', 33, 8, 32, 70, 155, 74, 'Brazil', 'AlexVolkmma.png'),
-('Kamaru Usman', 20, 2, 35, 72, 170, 76, 'Nigeria', 'AlexVolkmma.png'),
-('Francis Ngannou', 17, 3, 35, 76, 257, 83, 'Cameroon', 'AlexVolkmma.png'),
-('Leon Edwards', 20, 3, 31, 72, 170, 74, 'Jamaica', 'AlexVolkmma.png'),
-('Alamain Sterling', 21, 3, 33, 67, 134, 71, 'United States', 'AlexVolkmma.png'),
-('Dustin Poirier', 28, 7, 33, 69, 154, 72, 'United States', 'AlexVolkmma.png'),
-('Deiveson Figueiredo', 21, 2, 34, 65, 125, 68, 'Brazil', 'AlexVolkmma.png'),
-('Jiri Prochazka', 29, 3, 29, 75, 205, 80, 'Czechia', 'AlexVolkmma.png'),
-('Max Holloway', 23, 7, 30, 71, 144, 69, 'Hawaii', 'AlexVolkmma.png'),
-('Jon Jones', 26, 1, 34, 76, 204, 84, 'United States', 'AlexVolkmma.png'),
-('Stipe Miocic', 20, 4, 39, 77, 234, 80, 'United States', 'AlexVolkmma.png'),
-('Petr Yan', 16, 3, 29, 67, 134, 67, 'Russia', 'AlexVolkmma.png'),
-('Brandon Moreno', 20, 6, 28, 67, 124, 70, 'Mexico', 'AlexVolkmma.png');
+('Alexander Volkanovski', 25, 1, 33, 66, 144, 71, 'Australia', 'https://i.ibb.co/yBw8XLS/0984640e-8ef9-4283-afc3-d64ac0a0240a-VOLKANOVSKI-ALEXANDER-L-BELT-04-09.png'),
+('Israel Adesanya', 23, 1, 32, 76, 185, 80, 'Nigeria', 'https://i.ibb.co/cFDv1SM/68129-profile-galery-fullbodyleft-picture-ADESANYA-ISRAEL-L-06-12.png'),
+('Charles Oliveira', 33, 8, 32, 70, 155, 74, 'Brazil', 'https://i.ibb.co/7Ynw22T/Ee8b505b-b206-4f1e-9f72-36078790de50-jpg.webp'),
+('Kamaru Usman', 20, 2, 35, 72, 170, 76, 'Nigeria', 'https://i.ibb.co/kgJYCZV/12e8aab1-9c04-4234-9d4c-101ed57eb109-jpg.webp'),
+('Francis Ngannou', 17, 3, 35, 76, 257, 83, 'Cameroon', 'https://i.ibb.co/zhFDDPG/3d75a594-27a1-4c19-875a-8d7d4dc17b42-NGANNOU-FRANCIS-L-BELT-01-22.png'),
+('Leon Edwards', 20, 3, 31, 72, 170, 74, 'Jamaica', 'https://i.ibb.co/ZNmkfny/Faq-Pf-GJWQAAv-FLD.jpg'),
+('Alamain Sterling', 21, 3, 33, 67, 134, 71, 'United States', 'https://i.ibb.co/NpDc8n7/STERLING-ALJAMAIN-L-BELT-04-09.png'),
+('Dustin Poirier', 28, 7, 33, 69, 154, 72, 'United States', 'https://i.ibb.co/zscFgP4/NINTCHDBPICT000631778821-jpg.webp'),
+('Deiveson Figueiredo', 21, 2, 34, 65, 125, 68, 'Brazil', 'https://i.ibb.co/KVdmrrz/67787-profile-galery-fullbodyleft-picture-FIGUEIREDO-DEIVESON-L-BELT-11-21.png'),
+('Jiri Prochazka', 29, 3, 29, 75, 205, 80, 'Czechia', 'https://i.ibb.co/f1VBH6M/E66264ab-cd89-48bb-a170-1fec589b46a5-PROCHAZKA-JIRI-L-BELTCOMP.png'),
+('Max Holloway', 23, 7, 30, 71, 144, 69, 'Hawaii', 'https://i.ibb.co/Cvc1CdN/49b91c96-87a8-43ce-af9b-f9cac2c394d0-HOLLOWAY-MAX-L-07-02.png'),
+('Jon Jones', 26, 1, 34, 76, 204, 84, 'United States', 'https://i.ibb.co/NxL2pBR/9d9da43eb85ccf6038a2820166f68f9d.jpg'),
+('Stipe Miocic', 20, 4, 39, 77, 234, 80, 'United States', 'https://i.ibb.co/pdjJbcX/MIOCIC-STIPE-L.png'),
+('Petr Yan', 16, 3, 29, 67, 134, 67, 'Russia', 'https://i.ibb.co/HxWgFgM/69008-profile-galery-fullbodyleft-picture-YAN-PETR-L-10-30.png'),
+('Brandon Moreno', 20, 6, 28, 67, 124, 70, 'Mexico', 'https://i.ibb.co/ZzLxWzB/MORENO-BRANDON-L-BELT-01-22.png');
