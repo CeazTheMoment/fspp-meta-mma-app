882e3750c6e3acbf35f79b530e27fdf91bb04270
Cesar Ortiz <ceazthemoment@aol.com>
2022-09-03 03:00:11 -0400

    deploy

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hello World</code>
         </p>
-        <a
-          className="App-link"
-          href="https://damp-shore-75870.herokuapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
